7f3084d3acf38735e43b212b0c5b5916f4b413d1
nhucuc9999 <nhucuc9999@gmail.com>
2026-02-06 21:37:12 +0700

    fix: add rotate_tasks dict for auto-refresh task management

--- src/gemini_webapi/utils/__init__.py
+++ src/gemini_webapi/utils/__init__.py
@@ -1,5 +1,7 @@
 # flake8: noqa
 
+from asyncio import Task
+
 from .decorators import running
 from .get_access_token import get_access_token
 from .load_browser_cookies import load_browser_cookies
@@ -7,3 +9,6 @@ from .logger import logger, set_log_level
 from .parsing import extract_json_from_response, get_nested_value, parse_stream_frames
 from .rotate_1psidts import rotate_1psidts
 from .upload_file import upload_file, parse_file_name
+
+# Dictionary to store auto-refresh asyncio tasks by __Secure-1PSID
+rotate_tasks: dict[str, Task] = {}
